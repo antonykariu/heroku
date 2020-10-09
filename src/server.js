d9e00357d98014a7897b86db5352006c85997d4b
Antony_Kariu <kariuantony@gmail.com>
2020-10-09 18:41:28 +0300

    upstream create

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use("/", (req, res, next) => {
-  res.status(200).json({ message: "successful" });
+  res.status(200).json({ message: "successful create upstream branch" });
 });
 
 app.listen(port, () => console.log(`running on port ${port}`));
